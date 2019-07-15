10c30fefdfda046c11134a505644d8d360d8ae5c
wijdan1995 <wijdan.w.k@outlook.sa>
2019-07-15 11:18:05 +0300

    solved

--- bin/app-students.js
+++ bin/app-students.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://localhost/mongoose-crud', {
 const db = mongoose.connection
 
 //Here, Import the `models/student.js` to variable and called 'Student'
-// const Student = ...??
+const Student = require('../models/student.js');
 
 const done = function () { // eslint-disable-line no-unused-vars
   db.close()
@@ -17,27 +17,78 @@ const done = function () { // eslint-disable-line no-unused-vars
 
 const create = function (firstName, lastName, grade, age, city) {
   /* Add Code Here */
+  // node bin/app-students.js create firstname last grade age city
+  // to get and set the values
+  const studentParams = {
+    firstName: firstName,
+    lastName: lastName,
+    grade: grade,
+    age: age,
+    city: city
+  }
+  //the method create using the params
+  Student.create(studentParams)
+    // to print what we saved
+    .then((student) => console.log(student.toJSON()))
+    // if there is any errors
+    .catch(console.error)
+    // to close the database -> get out of mongodb
+    .then(done)
 }
 
 const index = function () {
   /* Add Code Here */
-  
+  // find all
+  Student.find()
+    // for each one console log
+    .then(students => {
+      students.forEach(student => console.log(student.toJSON()))
+    })
+    .catch(console.error)
+    .then(done)
 }
 
 const show = function (id) {
   /* Add Code Here */
+  // find only one by id 
+  Student.findById(id)
+    // console log the resulte
+    .then(student => console.log(student.toJSON()))
+    .catch(console.error)
+    .then(done)
 }
 
 const destroy = function (id) {
   /* Add Code Here */
+  // find by id
+  Student.findById(id)
+    // delete what it find
+    .then(student => student.remove())
+    .catch(console.error)
+    .then(done)
 }
 
 const update = function (id, field, value) {
   /* Add Code Here */
+  // to run node bin/app-students.js update (id) (field) (value)
+  //node bin/app-students.js update 5d2c2fb241686f4a302e142f grade 99
+
+  // find by id
+  Student.findById(id)
+    // get the document and student[age] student[grade] the field is the name of the key then save 
+    .then(student => {
+      student[field] = value
+      return student.save()
+    })
+    // to show 
+    .then(student => console.log(student.toJSON()))
+    .catch(console.error)
+    .then(done)
+
 }
 
 
- 
+
 db.once('open', function () {
   const command = process.argv[2]
 
--- models/student.js
+++ models/student.js
@@ -1,32 +1,42 @@
 'use strict'
 
 //- import the 'mongoose' 
-
+const mongooes = require('mongoose');
 // The new schema, here
-    //- Create new Schema called 'studentSchema'
+//- Create new Schema called 'studentSchema'
+const studentSchema = new mongooes.Schema({
     // * firstName - (String, required)
+    firstName: {
+        type: String,
+        required: true
+    },
     // * lastName - (String, required)
+    lastName: {
+        type: String,
+        required: true
+    },
     // * grade - (Number, required)
+    grade: {
+        type: Number,
+        required: true
+    },
     // * age - (Number, Greater than or equal 18)
+    age: {
+        type: Number,
+        min: 18
+    },
     // * city - (String)
-
-
-
-
-
-
-
-
-
-
-    
-
+    city: {
+        type: String
+    }
+},
+    {
+        timestamps: true
+    })
 
 // The model of the schema 
-
-
-
+const Student = mongooes.model("Student", studentSchema)
 
 // Export the model 
-
+module.exports = Student
 
